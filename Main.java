ccf8a567079b22d9cef8d3640e98654f96cc9d61
tumn97 <s325862@oslomet.no>
2018-10-01 08:13:01 +0200

    Oppgave 0, og litt av oppgave 1

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Algoritme.oblig.oblig2;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Liste<String> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.antall() + " " + liste.tom());
+
+        String[] s = {null,null,null};
+        Liste<String> liste1 = new DobbeltLenketListe<>(s);
+
+        System.out.println(liste1.antall() + " "+ liste1.tom());
+
+    }
+
+
+
+}
